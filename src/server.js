732a64fcb0d2f6b396f25d46bc233caadb238dc0
prashant-restyles <prashant@restyles.nl>
2026-09-18 11:24:08 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import http from "node:http";
+import { add } from "./app.js";
+
+const server = http.createServer((req, res) => {
+  res.end(`2 + 3 = ${add(2, 3)}\n`);
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
---
+++ tests/app.test.js
@@ -0,0 +1,7 @@
+import { test } from "node:test";
+import assert from "node:assert";
+import { add } from "../src/app.js";
+
+test("add 2 + 3 = 5", () => {
+  assert.strictEqual(add(2, 3), 5);
+});
