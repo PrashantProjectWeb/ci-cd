01d9c8511dcb29aa81870c74acc5da5f3d7d08f5
prashant-restyles <prashant@restyles.nl>
2026-09-21 12:21:47 +0530

    version 2 message for real

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import http from "node:http";
 import { add } from "./app.js";
-//  res.end(`VERSION 2 — 2 + 3 = ${add(2, 3)}\n`);
+
 
 const server = http.createServer((req, res) => {
-  res.end(`2 + 3 = ${add(2, 3)}\n`);
+   res.end(`VERSION 2 — 2 + 3 = ${add(2, 3)}\n`);
 });
 
 server.listen(3000, () => console.log("Server running on port 3000"));
