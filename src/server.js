d81ff530d2beb35ee423cd89d3e6802a369bf1a2
prashant-restyles <prashant@restyles.nl>
2026-09-21 12:11:00 +0530

    version 2 message

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "node:http";
 import { add } from "./app.js";
+//  res.end(`VERSION 2 — 2 + 3 = ${add(2, 3)}\n`);
 
 const server = http.createServer((req, res) => {
   res.end(`2 + 3 = ${add(2, 3)}\n`);
