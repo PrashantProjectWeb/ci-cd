c322f3198adcb4b39bfe26df94801a7721df52f3
prashant-restyles <prashant@restyles.nl>
2026-09-22 13:05:38 +0530

    fail-fast

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,9 @@
 import { test } from "node:test";
 import assert from "node:assert";
 import { add } from "../src/app.js";
-import os from "node:os";
+
 
 test("add 2 + 3 = 5", () => {
   assert.strictEqual(add(2, 3), 5);
 });
 
-test("sirf windows par pass hoga", () => {
-  assert.strictEqual(os.platform(), "win32");
-})
